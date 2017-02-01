74d4e04b3339e06565f01ea60890159e6533b0e5
qblol <iqbal.0809@gmail.com>
2017-02-01 16:10:21 +0700

    add owm api

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ const config = require('./config');
 const googleMapsClient = require('@google/maps').createClient({
   key: config.googleKey
 });
+const weather = require('npm-openweathermap');
+weather.api_key = config.owmKey;
+weather.temp = 'c';
 
 // Geocode an address.
 router.get('/search', function(req,res) {
   var params = {address: req.query.place}
+  var city
+  var lon
+  var lat
   googleMapsClient.geocode(params, function(err, response) {
     if (!err) {
       res.send(response.json.results);
+      // city = response.json.results[0].address_components[0].short_name
+      // weather.get_weather_custom('city', city, 'weather').then(function(hasil){
+      //     res.send(hasil);
+      // })
+
     }
-  });
+  })
 })
 
 app.use('/', router)
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  owmKey:               ''
+  owmKey:               'ad65ab49eba391c0aeaa6071a23867c6',
   googleKey:            'AIzaSyBANPwyCOZc4cE_XHCE_h4icIPa4F8-qsU'
 }
